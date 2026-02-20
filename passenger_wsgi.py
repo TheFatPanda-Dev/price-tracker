ace828cb57ce638ffb71b47eb1e11cb06d0517cf
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2026-02-20 12:54:35 +0100

    Simplify passenger_wsgi.py - let NeoServ handle venv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,10 +2,6 @@ import sys
 import os
 
 # Add your application directory to the Python path
-INTERP = os.path.join(os.environ['HOME'], 'price-tracker', 'venv', 'bin', 'python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Import the Flask app
